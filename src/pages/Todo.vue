19ec3e50caa412b587b2064c2209a088034045c3
Serg <0webdeveloper@gmail.com>
2021-12-01 21:03:24 +0300

    added filter todos

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -3,9 +3,15 @@
     <h2>Todolist components</h2>
     <CounterTodo :arrTodos="arrTodos"/>
     <AddForm @add-todo="addTodo"/>
+
+    <p class="filter-title">Todo filter</p>
+    <select class="filterTodo" v-model="filter">
+      <option v-for="value in select" :value="value">{{ value }}</option>
+    </select>
+
     <Loader v-if="loading"/>
-    <TodoList :todos="arrTodos"
-              v-else-if="arrTodos.length"/>
+    <TodoList :todos="filterTodos"
+              v-else-if="filterTodos.length"/>
     <p v-else>No Todos or all todos is done! Congratulations!</p>
   </div>
 </template>
@@ -23,18 +29,32 @@ export default {
   data() {
     return {
       arrTodos: [],
-      loading: true
+      loading: true,
+      select: ['all', 'completed', 'active'],
+      filter: 'all'
     }
   },
   mounted() {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=20')
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
         .then(response => response.json())
         .then(json => {
-          setTimeout(() => {
-            this.arrTodos = json;
-            this.loading = false;
-          },1000)
+          this.arrTodos = json;
+          this.loading = false;
         });
+  },
+  computed: {
+    filterTodos() {
+      console.log(this.filter)
+      if (this.filter === 'all') {
+        return this.arrTodos;
+      }
+      if (this.filter === 'completed') {
+        return this.arrTodos.filter(item => item.completed);
+      }
+      if (this.filter === 'active') {
+        return this.arrTodos.filter(item => !item.completed);
+      }
+    }
   },
   created() {
     eventBus.$on('remTodo', data => {
@@ -51,3 +71,34 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.filter-title {
+  margin-bottom: 20px;
+}
+
+.filterTodo {
+  display: block;
+  font: 700 16px sans-serif;
+  color: #444;
+  line-height: 1.3;
+  outline: none;
+  padding: 10px;
+  width: 100%;
+  max-width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 30px;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0, 0, 0, .04);
+  border-radius: 5px;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #fff;
+  background-image: url(data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E), linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+  background-repeat: no-repeat, repeat;
+  background-position: right .7em top 50%, 0 0;
+  background-size: .65em auto, 100%;
+}
+
+</style>
